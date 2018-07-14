b14d045e54709266dfd0ecc7f2cb000c6e9d31f0
John Coben <jcoben@gmail.com>
2018-07-14 14:39:06 -0400

    Implemented Decorator pattern

---
+++ Decorator/src/main/java/ToppingDecorator.java
@@ -0,0 +1,18 @@
+
+abstract class ToppingDecorator implements Pizza{
+
+	protected Pizza tempPizza;
+	
+	public ToppingDecorator(Pizza newPizza) {
+		tempPizza = newPizza;
+	}
+	
+	@Override
+	public String getDescription() {
+		return tempPizza.getDescription();
+	}
+	
+	public double getCost() {
+		return tempPizza.getCost();
+	}
+}
