ff50ac351ae6e2602d1b4b1af02f248f6cd2361a
John Coben <jcoben@gmail.com>
2018-07-16 18:46:53 -0400

    Implemented Bridge pattern

---
+++ Bridge/src/main/resources/EntertainmentDevice.java
@@ -0,0 +1,21 @@
+
+abstract class EntertainmentDevice {
+	
+	public int deviceState;
+	public int maxSetting;
+	public int volumeLevel = 0;
+	
+	public abstract void buttonFivePressed();
+	public abstract void buttonSixPressed();
+	
+	public void deviceFeedback() {
+		if (deviceState > maxSetting || deviceState < 0) {
+			System.out.println("On " + deviceState);
+		}
+	}
+	
+	public void buttonSevenPressed() {
+		volumeLevel++;
+		System.out.println("Volume at " + volumeLevel);
+	}
+}
